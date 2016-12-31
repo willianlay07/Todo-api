c999ca59686cce5ddadd367b89de8415a7333295
willianlay07 <willianlay07@gmail.com>
2017-01-01 00:47:51 +0800

    Final status todos

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', function(req, res){
 	res.send('Hello - Todo API Root');
 });
 
-// Get /todos or /todos?completed=true
+// Get /todos or /todos?completed=true or /todos?completed=true&q=house
 app.get('/todos', function(req, res){
 	var queryParams 	= req.query;
 	var filteredTodos 	= todos;
@@ -24,6 +24,14 @@ app.get('/todos', function(req, res){
 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
 		filteredTodos 	= _.where(filteredTodos, {completed: false});
 	}
+
+
+	if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0){
+		filteredTodos 	= _.filter(filteredTodos, function(todo){
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
+	}
+
 
 	res.json(filteredTodos);
 	//res.json(todos);
