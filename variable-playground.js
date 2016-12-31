144b95ca2c8e6341984687d22c59b8a60a31dcb5
willianlay07 <willianlay07@gmail.com>
2016-12-31 23:47:22 +0800

    Add veriable playground area

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+// var person 	= {
+// 	name : 'Wai',
+// 	age: 28
+// };
+
+// function updatePerson(obj){
+// 	// obj 	= {
+// 	// 	name: 'Wa',
+// 	// 	age: 21
+// 	// };
+
+// 	obj.age = 21;
+// }
+
+// updatePerson(person);
+// console.log(person);
+// Array;
+var grades 	= [15, 88];
+
+function addGrades(gradesArr){
+	gradesArr.push(55);
+	debugger;
+
+	//gradesArr 	= [12, 33, 99];
+}
+
+addGrades(grades);
+console.log(grades);
