f52b6b3c817656be79c6f48e2d0c6b522fd7d0f3
willianlay07 <willianlay07@gmail.com>
2017-01-02 19:38:24 +0800

    Convert GET /todos to sequelize

--- server.js
+++ server.js
@@ -37,31 +37,6 @@ app.get('/todos', function(req, res) {
 		res.status(500).send();
 	});
 
-
-
-	// Normal insert to Array without DB;
-	//var queryParams = req.query;
-	// var filteredTodos = todos;
-
-	// if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-	// 	filteredTodos = _.where(filteredTodos, {
-	// 		completed: true
-	// 	});
-	// } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-	// 	filteredTodos = _.where(filteredTodos, {
-	// 		completed: false
-	// 	});
-	// }
-
-
-	// if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-	// 	filteredTodos = _.filter(filteredTodos, function(todo) {
-	// 		return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-	// 	});
-	// }
-
-	//res.json(filteredTodos);
-	//res.json(todos);
 });
 
 // Get /todos/:id
@@ -80,18 +55,6 @@ app.get('/todos/:id', function(req, res) {
 		res.status(500).send();		// Server Error;
 	});
 
-	// Normal insert to Array without DB;
-	// var matchedTodo = _.findWhere(todos, {
-	// 	id: todoId
-	// });
-
-	// if (matchedTodo) {
-	// 	res.json(matchedTodo);
-	// } else {
-	// 	res.status(404).send();
-	// }
-
-	//res.send('Asking for todo with id of ' + req.params.id);
 });
 
 
@@ -105,22 +68,6 @@ app.post('/todos', function(req, res) {
 		res.status(400).json(e);
 	});
 
-	// Normal insert to Array without DB;
-	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	// 	return res.status(400).send();
-	// }
-
-	// body.description = body.description.trim();
-
-	// body.id = todoNextId;
-	// todoNextId++;
-
-	// // Push body into a array;
-	// todos.push(body);
-
-	// console.log('description : ' + body.description);
-
-	// res.json(body);
 });
 
 
