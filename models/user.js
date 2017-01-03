90417a87c6459649c2daadbb096742ea05cc3a50
willianlay07 <willianlay07@gmail.com>
2017-01-04 00:19:23 +0800

    Convert email to lowercase

--- models/user.js
+++ models/user.js
@@ -15,6 +15,14 @@ module.exports 	= function(sequelize, DataTypes){
 				len: [7, 100]
 			}
 		}
+	}, {
+		hooks: {
+			beforeValidate: function(user, options){
+				if(typeof user.email === 'string'){
+					user.email 	= user.email.toLowerCase();
+				}
+			}
+		}
 	});
 };
 
