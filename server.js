f54587e9de25a23f68b49da715de8fb719005567
willianlay07 <willianlay07@gmail.com>
2017-01-08 13:01:41 +0800

    Add auth middleware findbytoken

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var _ 				= require('underscore');
 var db 				= require('./db.js');
 var bcrypt 			= require('bcrypt');
 
+var middleware 		= require('./middleware.js')(db);
+
 var app 			= express();
 var PORT 			= process.env.PORT || 3000;
 var todos 			= [];
@@ -16,7 +18,7 @@ app.get('/', function(req, res) {
 });
 
 // Get /todos or /todos?completed=true or /todos?completed=true&q=house
-app.get('/todos', function(req, res) {
+app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	var query 		= req.query;
 	var where 		= {};
 
@@ -43,7 +45,7 @@ app.get('/todos', function(req, res) {
 });
 
 // Get /todos/:id
-app.get('/todos/:id', function(req, res) {
+app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	
 	db.todo.findById(todoId).then(function(ttdd){
@@ -62,7 +64,7 @@ app.get('/todos/:id', function(req, res) {
 
 
 // POST /todos
-app.post('/todos', function(req, res) {
+app.post('/todos', middleware.requireAuthentication, function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
 	db.todo.create(body).then(function(todo){
@@ -74,7 +76,7 @@ app.post('/todos', function(req, res) {
 });
 
 
-app.delete('/todos/:id', function(req, res) {
+app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 		
 	db.todo.destroy({
@@ -96,7 +98,7 @@ app.delete('/todos/:id', function(req, res) {
 
 
 // PUT;
-app.put('/todos/:id', function(req, res) {
+app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId 		= parseInt(req.params.id, 10);
 	var body 		= _.pick(req.body, 'description', 'completed');
 	var attribute 	= {};
